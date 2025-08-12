026f4262233672cbc4ea0d22818a51a287d6d538
Mercedes Martinez <mercedesviolin@gmail.com>
2025-08-12 15:38:24 -0300

    probando imagen de fondo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,16 +12,16 @@ export default function Hero() {
         gsap.fromTo(
             heroRef.current,
             {
-                clipPath: "circle(0% at 50% 50%)" // empieza como un punto en el centro
+                clipPath: "circle(0% at 50% 50%)" // empieza como punto
             },
             {
-                clipPath: "circle(150% at 50% 50%)", // crece hasta cubrir todo
+                clipPath: "circle(150% at 50% 50%)", // crece
                 ease: "power2.out",
                 scrollTrigger: {
                     trigger: heroRef.current,
                     start: "top top",
-                    end: "+=500", // distancia de scroll para que sea progresivo
-                    scrub: 2
+                    end: "+=500",
+                    scrub: 4
                 }
             }
         );
@@ -29,9 +29,7 @@ export default function Hero() {
 
     return (
         <section className="hero" ref={heroRef} id="inicio">
-            <div className="hero-content">
-              <p className="hero-p">Mi enfoque integral en la gestión cultural abarca desde la producción hasta la interpretación musical. Como manager, productora y violoncellista, lidero proyectos que conectan a los artistas con oportunidades claves para su desarrollo. Al seguir activa como violoncellista freelance y docente, me mantengo en constante contacto con el mundo artístico, lo que me permite aportar una visión fresca y auténtica a cada proyecto</p>
-            </div>
+            {/* La imagen será el fondo */}
         </section>
     );
 }
